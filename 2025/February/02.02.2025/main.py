137ef7556d3b3d65135e0f2e2d828975f152a6bd
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-02-02 15:04:26 +0200

    Enhance logging in Selenium automation tests with timestamped log files and improved button element handling

--- 2025/February/02.02.2025/main.py
+++ 2025/February/02.02.2025/main.py
@@ -7,7 +7,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 import logging
 
-logging.basicConfig(level=logging.INFO)
+now_time = datetime.now().strftime("%H:%M:%S")
+
+logging.basicConfig(
+    filename=f"app_test_{now_time}.log",  # Log messages will be written to 'app.log'
+    filemode="w",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 logging.info("Starting Automation Test... ✅")
 service = Service()
@@ -18,7 +25,7 @@ driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("http://localhost:5173")
 
-now_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+now_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 driver.save_screenshot(f"./screenshots/page_full_screen_{now_time}.png")
 
 # Assert page title
@@ -29,10 +36,13 @@ logging.info("The page title is correct! ✅")
 try:
     logging.info("Checking the button...")
     mail_input = WebDriverWait(driver, 3).until(
-        EC.presence_of_element_located((By.XPATH, "//input[@id=':r3:']"))
+        EC.presence_of_element_located((By.XPATH, "//input[@id=':3:']"))
     )
+
+    logging.debug("Button found!")
+
     mail_input.screenshot("./test.png")
-    
+
     assert mail_input.is_displayed(), "The Button disn't displayed"
     logging.info("The button is displayed! ✅")
 except AssertionError as e:
