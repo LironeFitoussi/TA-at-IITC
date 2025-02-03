3e01355a94546c3003f31648d90937c2572a0c27
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-02-03 10:34:25 +0200

    Refactor Selenium test to include JS alert and confirm button handling with improved logging

--- 2025/February/03.02.2025/main.py
+++ 2025/February/03.02.2025/main.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 import logging
+import time
 
 now_time = datetime.now().strftime("%H:%M:%S")
 
@@ -25,5 +26,57 @@ driver = webdriver.Chrome(service=service, options=options)
 
 logging.info("Navigating to the URL... ✅")
 driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+
+# Get Js Alert Button
+try: 
+    js_alert_btn = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//ul/li[1]/button'))
+    )
+    
+    assert js_alert_btn.is_displayed, "Error Not Found Button! 🆘"
+    logging.info("Button Found... ✅")
+    
+    logging.info("Clicking Button...")
+    
+    js_alert_btn.click()
+    
+    logging.info("Alert Button Clicked!✅")
+    
+    alert = driver.switch_to.alert
+    assert "I am a JS Alert" in alert.text, "Text Does not match the alert"
+    logging.info("Text Does match the alert!✅")    
+    alert.accept()
+    driver.switch_to.window
+except TimeoutException:
+    logging.error("Button Wasnt Found in Time! 🆘")
+except AssertionError as e:
+    logging.error(e)
+
+# Get JS Confirm Button
+try: 
+    js_confirm_btn = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//ul/li[2]/button'))
+    )
+    
+    assert js_confirm_btn.is_displayed, "Error Not Found Button! 🆘"
+    logging.info("Button Found... ✅")
+    
+    logging.info("Clicking Button...")
+    
+    js_confirm_btn.click()
+    
+    logging.info("Alert Button Clicked!✅")
+    
+    alert = driver.switch_to.alert
+    
+    assert "I am a JS Confirm" in alert.text, "Text Does not match the alert"
+    logging.info("Text Does match the alert!✅")
+    alert.accept()
+    driver.switch_to.window
+    # time.sleep(5)
+except TimeoutException:
+    logging.error("Button Wasnt Found in Time! 🆘")
+except AssertionError as e:
+    logging.error(e)
 
 logging.info("Automation Test Done! ✅")
