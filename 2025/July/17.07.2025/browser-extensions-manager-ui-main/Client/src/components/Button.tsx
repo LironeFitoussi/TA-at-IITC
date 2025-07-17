7565c8cce828b201e530df5c6d2ea36c5f5e3bd4
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-07-17 16:10:55 +0300

    Refactor Button and Card components for improved functionality and styling
    - Updated Button component to accept props for variant, size, and title, enhancing its reusability.
    - Integrated Button component into Card, replacing the static remove button with a dynamic Button that triggers an alert.
    - Adjusted Card layout to include flex properties for better alignment and spacing.

--- 2025/July/17.07.2025/browser-extensions-manager-ui-main/Client/src/components/Button.tsx
+++ 2025/July/17.07.2025/browser-extensions-manager-ui-main/Client/src/components/Button.tsx
@@ -1,10 +1,21 @@
+interface ButtonProps {
+    variant?: string;
+    onClick: () => void;
+    size: 's' | 'm' | 'l'
+    title: string
+}
 
-interface ButtonProps
+const ButtonStyle = {
+    'primary': ''
+}
 
-export default function Button () {
+
+export default function Button ({ variant = 'primary', onClick, size, title}: ButtonProps) {
+    
+    
     return (
-        <button>
-            
+        <button className="rounded-4xl bg-[hsl(var(--neutral-600))] p-2">
+            {title}
         </button>
     )
 }
--- 2025/July/17.07.2025/browser-extensions-manager-ui-main/Client/src/components/Card.tsx
+++ 2025/July/17.07.2025/browser-extensions-manager-ui-main/Client/src/components/Card.tsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 interface CardProps {
     logo: string;
     name: string;
@@ -10,7 +12,7 @@ export default function Card({ logo, name, description, isActive }: CardProps) {
     const reformattedLogo = logo.replace(/^\.\//, "/src/");
     
     return (
-        <div className="flex flex-col text-white bg-[hsl(var(--neutral-700))] p-4 rounded-xl">
+        <div className="flex flex-col text-white bg-[hsl(var(--neutral-700))] p-4 rounded-xl justify-between h-[20vh]">
             <div className="flex gap-4 items-center">
                 <img src={reformattedLogo} alt="" />
                 <div>
@@ -18,8 +20,13 @@ export default function Card({ logo, name, description, isActive }: CardProps) {
                     <p className="text-sm">{description}</p>
                 </div>
             </div>
-            <div>
-                <button>remove</button>
+            <div className="flex justify-between items-center">
+                <Button 
+                    variant="outline"
+                    onClick={() => alert('CardButton')}
+                    size="s"
+                    title="Remove"
+                />
                 <button>switch</button>
             </div>
         </div>
