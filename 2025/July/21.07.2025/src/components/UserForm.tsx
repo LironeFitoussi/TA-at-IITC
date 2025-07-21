2b50a504c35ecc062c03a628a62f5ea6f2765760
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-07-21 11:12:45 +0300

    Refactor UserForm component to use react-hook-form for improved validation and state management; update age validation to ensure numeric input and minimum age requirement.

--- 2025/July/21.07.2025/src/components/UserForm.tsx
+++ 2025/July/21.07.2025/src/components/UserForm.tsx
@@ -1,68 +1,39 @@
-import { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 
 const userSchema = z.object({
     name: z.string().min(2, "Name must contain at least 2 charcters"),
     email: z.email("Invalid email address"),
     password: z.string().min(8, "Password must be at least 8 characters"),
-    age: z.number().min(18, "Go to your mommy"),
+    age: z.string()
+            .regex(/^\d+$/, { message: "Must contain only digits" }) // only digits
+            .refine(val => +(val) >= 18, { message: "Must be at least 18" })
 })
 
 type UserFormData = z.infer<typeof userSchema>
 
 export function UserForm() {
-    const [formData, setFormData] = useState<UserFormData>({
-        name: '',
-        email: '',
-        password: '',
-        age: 0,
+    const { register, handleSubmit, formState: { errors, isSubmitting, isSubmitSuccessful}, reset} = useForm<UserFormData>({
+        resolver: zodResolver(userSchema),
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+            age: ''
+        }
     })
 
-    const [errors, setErrors] = useState<Record<string, string>>({})
 
-    const validateForm = (data: UserFormData) => {
-        const result = userSchema.safeParse(data);
 
-        if (result.success) {
-            setErrors({})
-            return true
-        } else {
-            const newErrors: Record<string, string> = {};
-            result.error.issues.forEach((err: any) => {
-                const field = err.path[0] as string;
-                if (field) newErrors[field] = err.message
-            })
-
-            setErrors(newErrors)
-            return false
-        }
-    }
-
-    // {
-    //     !name: '' (TO BE OVERWRITTEN)
-    //     email: '',
-    //     password: '',
-    //     age: 0,
-    //     name: "Lirone"
-    // }
-    
-    const handleInputChange = (field: keyof UserFormData, value: string | number) => {
-        const newFromData = { ...formData, [field]: value };
-        setFormData(newFromData)
-    }
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-
-        if (validateForm(formData)) {
-            alert(`Form is valid! ${JSON.stringify(formData, null, 2)}`)
-        } else {
-            alert('error')
-        }
+    const onSubmit = async (data: UserFormData) => {
+        await new Promise(resolve => setTimeout(resolve, 3000))
+        console.log(`Form is valid! Data: ${JSON.stringify(data, null, 2)}`);
+        reset()
     }
 
     return (
-        <form onSubmit={handleSubmit} className="bg-gray-50 p-8 rounded-xl shadow-sm mb-8">
+        <form onSubmit={handleSubmit(onSubmit)} className="bg-gray-50 p-8 rounded-xl shadow-sm mb-8">
             <div className="mb-6">
                 <label htmlFor="name" className="block text-sm font-semibold text-gray-700 mb-2">
                     Name:
@@ -70,13 +41,12 @@ export function UserForm() {
                 <input
                     id="name"
                     type="text"
-                    value={formData.name}
-                    onChange={(e) => handleInputChange('name', e.target.value)}
+                    {...register('name')}
                     className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors ${errors.name ? 'border-red-500 bg-red-50' : 'border-gray-300'
                         }`}
                 />
                 {errors.name && (
-                    <span className="text-red-600 text-sm mt-1 block">{errors.name}</span>
+                    <span className="text-red-600 text-sm mt-1 block">{errors.name.message}</span>
                 )}
             </div>
 
@@ -87,13 +57,12 @@ export function UserForm() {
                 <input
                     id="email"
                     type="email"
-                    value={formData.email}
-                    onChange={(e) => handleInputChange('email', e.target.value)}
+                    {...register('email')}
                     className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors ${errors.email ? 'border-red-500 bg-red-50' : 'border-gray-300'
                         }`}
                 />
                 {errors.email && (
-                    <span className="text-red-600 text-sm mt-1 block">{errors.email}</span>
+                    <span className="text-red-600 text-sm mt-1 block">{errors.email.message}</span>
                 )}
             </div>
 
@@ -104,13 +73,12 @@ export function UserForm() {
                 <input
                     id="password"
                     type="password"
-                    value={formData.password}
-                    onChange={(e) => handleInputChange('password', e.target.value)}
+                    {...register('password')}
                     className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors ${errors.password ? 'border-red-500 bg-red-50' : 'border-gray-300'
                         }`}
                 />
                 {errors.password && (
-                    <span className="text-red-600 text-sm mt-1 block">{errors.password}</span>
+                    <span className="text-red-600 text-sm mt-1 block">{errors.password.message}</span>
                 )}
             </div>
 
@@ -121,22 +89,22 @@ export function UserForm() {
                 <input
                     id="age"
                     type="number"
-                    value={formData.age}
-                    onChange={(e) => handleInputChange('age', parseInt(e.target.value) || 0)}
+                    {...register("age")}
                     className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-colors ${errors.age ? 'border-red-500 bg-red-50' : 'border-gray-300'
                         }`}
                 />
                 {errors.age && (
-                    <span className="text-red-600 text-sm mt-1 block">{errors.age}</span>
+                    <span className="text-red-600 text-sm mt-1 block">{errors.age.message}</span>
                 )}
             </div>
 
-            <button
+            { isSubmitSuccessful ? <p>Submitted</p> : <button
                 type="submit"
+                disabled={isSubmitting}
                 className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg font-semibold hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:bg-gray-400 disabled:cursor-not-allowed transition-colors"
             >
                 Submit Form
-            </button>
+            </button>}
         </form>
     )
 }
